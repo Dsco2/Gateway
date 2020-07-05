7b49df096bb376a9d241153fe52aaac6ec9ac820
Daniel Santiago Cantor <39808025+Dsco2@users.noreply.github.com>
2020-07-04 22:18:56 -0500

    Add scraper project

---
+++ Executable/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+//using Business;
+//using DataAccess;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Executable
+{
+    class Program
+    {
+        static void Main()
+        {
+            var services = ConfigureServices();
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            // calls the Run method in App, which is replacing Main
+            serviceProvider.GetService<App>().Run();
+        }
+
+        private static IServiceCollection ConfigureServices()
+        {
+            IServiceCollection services = new ServiceCollection();
+
+            var config = LoadConfiguration();
+            services.AddSingleton(config);
+
+            // required to run the application
+            services.AddTransient<App>();
+
+            // DI registration
+            //services.AddBusinessRegistrations();
+            //services.AddDataRegistrations(config);
+
+            return services;
+        }
+
+        public static IConfiguration LoadConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            return builder.Build();
+        }
+
+        
+    }
+}
---
+++ Executable/app.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Executable
+{
+    public class App
+    {
+
+        public void Run()
+        {
+            var driver = new ChromeDriver();
+            var url = "https://www.einforma.co/informes-empresas";
+            Console.WriteLine($"Scraping URL: {url}");
+            driver.Navigate().GoToUrl(url);
+            ProcessPage(driver);
+            driver.Quit();
+            Console.WriteLine("Console application finished running");
+        }
+
+        public static void ProcessPage(ChromeDriver driver)
+        {
+            var scrapedJobNode = driver.FindElement(By.XPath("//input[@id='search2']"));
+            scrapedJobNode.SendKeys("900806774");
+
+            driver.FindElement(By.XPath("//input[@id='boton_buscador_nacional']")).Click();
+
+            var Ici = driver.FindElement(By.XPath("//*[contains(text(),'ICI')]//ancestor::td//following-sibling::td[1]")).Text;
+            var Nit = driver.FindElement(By.XPath("//*[contains(text(),'Nit')]//ancestor::td//following-sibling::td[1]")).Text;
+            var RazonSocial = driver.FindElement(By.XPath("//*[contains(text(),'Razón Social')]//ancestor::td//following-sibling::td[1]")).Text;
+            var FormaJuridica = driver.FindElement(By.XPath("//*[contains(text(),'Forma Jurídica')]//ancestor::td//following-sibling::td[1]")).Text;
+            var Departamento = driver.FindElement(By.XPath("//*[contains(text(),'Departamento')]//ancestor::td//following-sibling::td[1]")).Text;
+            var DireccionActual = driver.FindElement(By.XPath("//*[contains(text(),'Dirección Actual')]//ancestor::td//following-sibling::td[1]")).Text;
+            var Telefono = driver.FindElement(By.XPath("//*[contains(text(),'Teléfono')]//ancestor::td//following-sibling::td[1]")).Text;
+            var Email = driver.FindElement(By.XPath("//*[contains(text(),'Email')]//ancestor::td//following-sibling::td[1]")).Text;
+            var ActividadCIIU = driver.FindElement(By.XPath("//*[contains(text(),'Actividad CIIU')]//ancestor::td//following-sibling::td[1]")).Text;
+            var FechaConstitucion = driver.FindElement(By.XPath("//*[contains(text(),'Fecha Constitución')]//ancestor::td//following-sibling::td[1]")).Text;
+            var MatriculaMercantil = driver.FindElement(By.XPath("//*[contains(text(),'Matrícula Mercantil')]//ancestor::td//following-sibling::td[1]")).Text;
+
+
+
+            Console.WriteLine($"{Ici}");
+            Console.WriteLine($"{Nit}");
+            Console.WriteLine($"{RazonSocial}");
+            Console.WriteLine($"{FormaJuridica}");
+            Console.WriteLine($"{Departamento}");
+            Console.WriteLine($"{DireccionActual}");
+            Console.WriteLine($"{Telefono}");
+            Console.WriteLine($"{Email}");
+            Console.WriteLine($"{ActividadCIIU}");
+            Console.WriteLine($"{FechaConstitucion}");
+            Console.WriteLine($"{MatriculaMercantil}");
+        }
+    }
+}
